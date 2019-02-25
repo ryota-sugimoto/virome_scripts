6772897263d8adfe5413312be9a54bcc16f9b4cb
Ryota Sugimoto <ryota.sugimoto@gmail.com>
2019-02-25 16:28:05 +0900

    added reverse complement

--- filter_blastout.py
+++ filter_blastout.py
@@ -19,23 +19,34 @@ for s in args.blastout:
   send = int(send)-1
   query_dict = dict([ s.split(':') for s in query.split(',')[1:] ])
   direct_repeat = Seq(query_dict['DR_seq'])
-  repeat_length = len(direct_repeat)
+  direct_repeat_rc = direct_repeat.reverse_complement()
   if sstart > send:
     sstart, send = send, sstart
+  repeat_length = len(direct_repeat)
   fivedash_adjacent = record_dict[subject][sstart-repeat_length: sstart].seq
   threedash_adjacent = record_dict[subject][send: send+repeat_length].seq
   score_function = lambda s1,s2: pairwise2.align.globalms(s1, s2,
                                                           1, -1, -1, -0.5,
                                                           score_only=True)
-  fivedash_adj_dr_score = score_function(fivedash_adjacent, direct_repeat)
-  threedash_adj_dr_score = score_function(threedash_adjacent, direct_repeat)
+  fivedash_adj_dr_score = score_function(fivedash_adjacent,
+                                         direct_repeat)
+  fivedash_adj_dr_rc_score = score_function(fivedash_adjacent,
+                                            direct_repeat_rc)
+  
+  threedash_adj_dr_score = score_function(threedash_adjacent,
+                                          direct_repeat)
+  threedash_adj_dr_rc_score = score_function(threedash_adjacent,
+                                             direct_repeat_rc)
+  
   fivedash_threedash_score = score_function(fivedash_adjacent,
                                             threedash_adjacent)
   
   threshold = repeat_length*0.5
-  if fivedash_adj_dr_score > threshold:
+  if fivedash_adj_dr_score > threshold \
+  or fivedash_adj_dr_rc_score > threshold:
     continue
-  elif threedash_adj_dr_score > threshold:
+  elif threedash_adj_dr_score > threshold \
+    or threedash_adj_dr_rc_score > threshold:
     continue
   elif fivedash_threedash_score > threshold:
     continue
