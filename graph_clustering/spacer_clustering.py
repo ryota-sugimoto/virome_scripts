6665813e7e9ea572a6620099dbf3187f3fbb6004
Ryota Sugimoto ddbj_server <rsugimot@nig.ac.jp>
2019-10-21 18:01:56 +0900

    added spacer clustering

--- graph_clustering/spacer_clustering.py
+++ graph_clustering/spacer_clustering.py
@@ -2,21 +2,28 @@
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('contig_protospacers',
+parser.add_argument('protospacers_bedfile',
                     type=argparse.FileType('r'))
 args = parser.parse_args()
 
 d = {}
-for s in args.contig_protospacers:
+for s in args.protospacers_bedfile:
   l = s.strip().split()
-  d[l[0]] = set(l[1].split(','))
+  contig = l[0]
+  spacer = '|'.join(sorted(l[3].split('|')))
+  if contig in d:
+    d[contig].add(spacer)
+  else:
+    d[contig] = set([spacer])
 
 from itertools import chain
-spacers = set(chain(*d.values()))
-spacers_id = {key:i for i,key in enumerate(sorted(list(spacers)))}
+import numpy as np
+np.set_printoptions(precision=3)
+spacers = np.array(sorted(list(set(chain(*d.values())))))
+spacers_id = {key:i for i,key in enumerate(spacers)}
 
-contigs = d.keys()
-contigs_id = {key:i for i,key in enumerate(sorted(list(contigs)))}
+contigs = np.array(sorted(d.keys()))
+contigs_id = {key:i for i,key in enumerate(contigs)}
 
 row = []
 col = []
@@ -25,25 +32,15 @@ for contig in contigs:
     row.append(contigs_id[contig])
     col.append(spacers_id[spacer])
 
-import numpy as np
-np.set_printoptions(precision=3)
 data = np.ones(len(row))
 
 from scipy.sparse import csr_matrix, find
-from scipy.sparse.csgraph import connected_components
 contig_spacer_mat = csr_matrix((data, (row,col)),
                                shape=(len(contigs), len(spacers)))
 
 spacer_cooccur_mat = contig_spacer_mat.T * contig_spacer_mat
 
-#TODO too slow
-'''
-nj = spacer_cooccur_mat.diagonal()
-ni = nj.reshape(-1,1)
-dice_coefficients = 2*spacer_cooccur_mat/(ni + nj)
-spacer_cooccur_mat[(dice_coefficients<min_sig).nonzero()] = 0
-spacer_cooccur_mat.eliminate_zeros()
-'''
+import igraph
 
 min_sig = 0.05
 i,j,v = find(spacer_cooccur_mat)
@@ -51,68 +48,72 @@ diag = spacer_cooccur_mat.diagonal()
 w = np.where( 2*v/(diag[i]+diag[j]) > min_sig, v, 0)
 spacer_cooccur_mat_ = csr_matrix((w, (i,j)),
                                  shape=spacer_cooccur_mat.shape)
+spacer_cooccur_mat_.setdiag(0)
 spacer_cooccur_mat_.eliminate_zeros()
+row, col = spacer_cooccur_mat_.nonzero()
+weight = spacer_cooccur_mat_.data
+g = igraph.Graph(list(zip(row.tolist(), col.tolist())),
+                 vertex_attrs={'name': spacers},
+                 edge_attrs={'weight': data.tolist()})
+components = g.components()
 
-from scipy.stats import describe
-n_components, labels = connected_components(spacer_cooccur_mat_, directed=False)
-print('n_components', n_components)
-unique, counts = np.unique(labels, return_counts=True)
+print('all_assort', g.assortativity_degree(directed=False))
+print('all_transitivity', g.transitivity_undirected())
+def cov_d(d, names):
+  keys = []
+  for name in names:
+    keys += list(key for key in d if name in d[key])
+  return len(set(keys))
 
-
-N = len(d.keys())
-for label,count in zip(unique,counts):
-  if count == 1:
+N = len(d)
+print('N', N)
+res = []
+for comp in components:
+  if len(comp) == 1:
+    res.append(['*' + g.subgraph(comp).vs['name'][0]])
     continue
-  print('component_label', label)
-  print('component_size', count)
-  index = (labels==label).nonzero()[0]
-  n = spacer_cooccur_mat_[index,:][:,index].todense()
-  n_diag = n.diagonal()
-  ni = n_diag.reshape(-1,1)
-  nj = n_diag
-  ll = np.multiply(N, np.log(N))
-  ll -= np.multiply(ni, np.ma.log(ni).filled(0)) \
-      + np.multiply(nj, np.ma.log(nj).filled(0))
-  ll += np.multiply(n, np.ma.log(n).filled(0))
-  
-  tmp = N - ni - nj + n
-  ll += np.multiply(tmp, np.ma.log(tmp).filled(0))
-  
-  tmp = ni - n
-  ll += np.multiply(tmp, np.ma.log(tmp).filled(0))
-  
-  tmp = nj - n
-  ll += np.multiply(tmp, np.ma.log(tmp).filled(0))
-  
-  tmp1 = N - ni
-  tmp2 = N - nj
-  ll -= np.multiply(tmp1, np.ma.log(tmp1).filled(0)) \
-      + np.multiply(tmp2, np.ma.log(tmp2).filled(0))
-  
-  ll *= 2
-  #ll = csr_matrix(ll)
-  print(ll)
 
+  comp_g = g.subgraph(comp)
+  print('N_comp', len(comp))
+  print('comp_assort', comp_g.assortativity_degree(directed=False))
+  print('comp_transitivity', comp_g.transitivity_undirected())
+  #communities = comp_g.community_label_propagation(weights='weight')
+  #communities = comp_g.community_multilevel(weights='weight')
+  #communities = comp_g.community_spinglass(weights='weight')
+  communities = comp_g.community_walktrap(weights='weight').as_clustering()
+  
+  for comm in communities:
+    N_comm = len(comm)
+    comm_g = comp_g.subgraph(comm)
+    comm_spacer_names = np.array(comm_g.vs['name'])
+    print('N_comm', N_comm)
+    print('comm_assort', comm_g.assortativity_degree(directed=False))
+    print('comm_transitivity', comm_g.transitivity_undirected())
+    
+    '''
+    cscores = np.array(comm_g.evcent(directed=False, weights='weight'))
+    print('eigenvalue', cscores, np.sum(cscores))
+    keyspacers_index = (cscores >= 0.9).nonzero()
+    keyspacers = set(comm_spacer_names[keyspacers_index])
+    print('eigenvalue', N_comm, len(keyspacers_index[0]), cov_d(d, keyspacers))
+    '''
+ 
+    cscores = np.array(comm_g.pagerank(directed=False, weights='weight'))
+    #print('pagerank', cscores/np.max(cscores), np.sum(cscores))
+    cscores /= np.max(cscores)
+    keyspacers_index = (cscores >= 0.9).nonzero()
+    keyspacers = comm_spacer_names[keyspacers_index]
+    #print('pagerank', N_comm, len(keyspacers_index[0]), cov_d(d, keyspacers))
 
+    '''
+    cscores = np.array(comm_g.degree(loops=False))
+    #keyspacers_index = np.argsort(cscores)[-min(N_comm, top_n):]
+    keyspacers_index = (cscores >= 0.8 * np.max(cscores)).nonzero()
+    keyspacers = comm_spacer_names[keyspacers_index]
+    print('degree', N_comm, len(keyspacers_index[0]), cov_d(d, keyspacers))
+    '''
 
-'''
-import networkx as nx
-g = nx.from_scipy_sparse_matrix(spacer_cooccur_mat)
-print('size', len(g))
-print('n_component', nx.number_connected_components(g))
-print('assortability', nx.degree_assortativity_coefficient(g))
-for c in nx.connected_components(g):
-  subg = g.subgraph(c)
-  print('size', len(c))
-  print('assortability', nx.degree_assortativity_coefficient(subg))
-  print('clustering', nx.average_clustering(subg))
-'''
+    res.append(list(name if name not in keyspacers else '*'+name for name in comm_spacer_names))
 
-'''
-import matplotlib.pyplot as plt
-f = plt.figure()
-nx.draw(g)
-plt.show()
-f.savefig('graph.pdf')
-plt.show()
-'''
+for names in res:
+  print(','.join(map(str,names)))
